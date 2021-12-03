cc61d92cdba01508734154f2ac089e7cf378bb3c
Omprakash Sridharan <omprakashsridharan@outlook.com>
2021-12-03 16:55:16 +0100

    Day 3

--- src/Day03.kt
+++ src/Day03.kt
@@ -1,63 +1,42 @@
 import kotlin.math.pow
 
-fun getOxygenRating(numbers: List<Int>, length: Int): Int{
+fun getRating(numbers: List<Int>, length: Int, reverse: Boolean = false): Int {
     var nums = numbers.toMutableList()
-    for (power in length - 1 downTo 0 ) {
+    for (power in length - 1 downTo 0) {
         val twoPower = 2.0.pow(power.toDouble()).toInt()
         var ones = 0
         var zeros = 0
         val numOnes = mutableListOf<Int>()
         val numZeroes = mutableListOf<Int>()
-        if(nums.size == 1){
+        if (nums.size == 1) {
             break
         }
         for (num in nums) {
             val res = num and twoPower
-            val bit = res/twoPower
-            if(bit == 0){
+            val bit = res / twoPower
+            if (bit == 0) {
                 zeros++
                 numZeroes.add(num)
-            }else{
+            } else {
                 ones++
                 numOnes.add(num)
             }
         }
-        if(ones >= zeros){
-            nums = numOnes
-        }else{
-            nums = numZeroes
+        nums = if (reverse) {
+            //CO2
+            if (ones >= zeros) {
+                numZeroes
+            } else {
+                numOnes
+            }
+        } else {
+            if (ones >= zeros) {
+                numOnes
+            } else {
+                numZeroes
+            }
         }
-    }
-    return nums[0]
-}
 
-fun getCo2Rating(numbers: List<Int>, length: Int): Int{
-    var nums = numbers.toMutableList()
-    for (power in length - 1 downTo 0 ) {
-        val twoPower = 2.0.pow(power.toDouble()).toInt()
-        var ones = 0
-        var zeros = 0
-        val numOnes = mutableListOf<Int>()
-        val numZeroes = mutableListOf<Int>()
-        if(nums.size == 1){
-            break
-        }
-        for (num in nums) {
-            val res = num and twoPower
-            val bit = res/twoPower
-            if(bit == 0){
-                zeros++
-                numZeroes.add(num)
-            }else{
-                ones++
-                numOnes.add(num)
-            }
-        }
-        if(ones >= zeros){
-            nums = numZeroes
-        }else{
-            nums = numOnes
-        }
     }
     return nums[0]
 }
@@ -68,7 +47,7 @@ fun main() {
     var l = 0
     for (line in report) {
         val num = line.toInt(2)
-        l = if(l < line.length) line.length else l
+        l = if (l < line.length) line.length else l
         numbers.add(num)
     }
     var result = ""
@@ -78,19 +57,19 @@ fun main() {
         var zeros = 0
         for (num in numbers) {
             val res = num and twoPower
-            val bit = res/twoPower
-            if(bit == 0){
+            val bit = res / twoPower
+            if (bit == 0) {
                 zeros++
-            }else{
+            } else {
                 ones++
             }
         }
-        result = (if(ones >= zeros) "1" else "0") + result
+        result = (if (ones >= zeros) "1" else "0") + result
     }
     val gamma = result.toInt(2)
     val powerConsumption = gamma * ((2.0.pow(l) - 1) - gamma)
     println(powerConsumption)
-    val oxygen = getOxygenRating(numbers,l)
-    val co2 = getCo2Rating(numbers,l)
-    println(oxygen*co2)
+    val oxygen = getRating(numbers, l)
+    val co2 = getRating(numbers, l, true)
+    println(oxygen * co2)
 }
